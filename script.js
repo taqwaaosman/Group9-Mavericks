0988053b09d1db853bf50af0b756c35175b6b8cb
Rita Ishimwe <ritaishimwe@cmail.carleton.ca>
2024-03-18 13:40:28 -0400

    Javascript code for the expensepage

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const transaction =[];
-const list = document.getElementById("")
+const list = document.getElementById("transactionList");
+function renderList(){
+    list.innerHTML = "<li>item</li>"
+}
+renderList();
