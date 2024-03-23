e7d597f2cb3ed9d1ee008b3da8b8d264cbe93df3
taqwaaosman <taqwaaosman@cmail.carleton.ca>
2024-03-23 14:18:27 -0400

    I forgot to add models.py

--- backend/MavericksApp/models.py
+++ backend/MavericksApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.source} - {self.amount}"
